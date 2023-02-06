db68bdb5b1f003013edc0c71b7f4f2feab9cbb18
Dev Kumar Pal <72178142+devkumar2313@users.noreply.github.com>
2023-02-06 16:44:59 +0530

    Delete devi4.cpp

--- devi4.cpp
+++
@@ -1,7 +0,0 @@
-
-Your Question:Why is processing a sorted array faster than processing an unsorted array?
-Your Answer:-
-
-Processing a sorted array is faster than processing an unsorted array because when the array is sorted, the elements are in order, which makes it easier to search for specific elements. This is because when the array is sorted, algorithms like binary search can be used to quickly find elements in the array. Binary search works by cutting the array in half and searching the appropriate half for the element. This process is repeated until the element is found. This is significantly faster than searching through an unsorted array, which requires searching through every element until the desired element is found.
-
-For example, if we wanted to search for the number 5 in the foll
